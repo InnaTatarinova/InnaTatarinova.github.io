2252f41f92246939cf3ac22245e46cbba6d5b67b
InnaTatarinova <inna.v.tatarinova@gmail.com>
2024-02-06 13:54:55 -0500

    add radio button for form's type

--- src/components/elementsComponent/FormForQuote/FormForFullQuote.js
+++ src/components/elementsComponent/FormForQuote/FormForFullQuote.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./FormForFullQuote.scss";
 import "./ElementsForQuote.scss";
 import { MovingFormContext } from "../../../context/MovingFormContext";
@@ -12,15 +12,21 @@ import {
 import { LocalResidentForm } from "./FormElementForQuote/LocalResidentForm";
 import { BedroomQuantityForm } from "./FormElementForQuote/BedroomQuantityForm";
 import { FloorForm } from "./FormElementForQuote/FloorForm";
-import { TruckSize} from "./FormElementForQuote/TruckSize";
+import { TruckSize } from "./FormElementForQuote/TruckSize";
 import { MoversQuantity } from "./FormElementForQuote/MoversQuantity";
 import { Hours } from "./FormElementForQuote/Hours";
 
 export const FormForFullQuote = () => {
   const methods = useForm();
 
-  const { movingFromInput, movingToInput, serviceType, residenceType} =
+  const { movingFromInput, movingToInput, serviceType, residenceType } =
     useContext(MovingFormContext);
+
+  const [selectedForm, setSelectedForm] = useState("full");
+
+  const handleRadioChange = (value) => {
+    setSelectedForm(value);
+  };
 
   return (
     <FormProvider {...methods}>
@@ -49,39 +55,65 @@ export const FormForFullQuote = () => {
           type="SERVICE_TYPE"
           form="full"
         />
-        <div className="residenceType">
-          {serviceType === null ? null : serviceType.id === 1 ? (
-            <LocalResidentForm />
-          ) : null}
+
+        <div className="formForChooseTypeOfForm">
+          <div>
+            <input
+              type="radio"
+              id="full"
+              value="full"
+              checked={selectedForm === "full"}
+              onChange={() => handleRadioChange("full")}
+            />
+            <label htmlFor="option1">Full form</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="short"
+              value="short"
+              checked={selectedForm === "short"}
+              onChange={() => handleRadioChange("short")}
+            />
+            <label htmlFor="option2">Short form</label>
+          </div>
         </div>
 
-        <div className="bedroomQuantity">
-          {residenceType === "House" ||
-          residenceType === "Apartment" ||
-          residenceType === "Condo" ? (
-            <BedroomQuantityForm />
-          ) : null}
-        </div>
+        {selectedForm === "full" ? (
+          <form>
+            <div className="residenceType">
+              {serviceType === null ? null : serviceType.id === 1 ? (
+                <LocalResidentForm />
+              ) : null}
+            </div>
 
-        <div className="floor">
-          {residenceType === "Apartment" || residenceType === "Condo" ? (
-            <FloorForm type="FLOOR_LEVEL" />
-          ) : null}
-        </div>
+            <div className="bedroomQuantity">
+              {residenceType === "House" ||
+              residenceType === "Apartment" ||
+              residenceType === "Condo" ? (
+                <BedroomQuantityForm />
+              ) : null}
+            </div>
 
-        <div className="truckSize">
-          <TruckSize/>
-        </div>
-
-        <div className="movers">
-          <MoversQuantity/>
-        </div>
-
-        <div className="hours">
-          <Hours/>
-        </div>
-
-
+            <div className="floor">
+              {residenceType === "Apartment" || residenceType === "Condo" ? (
+                <FloorForm type="FLOOR_LEVEL" />
+              ) : null}
+            </div>
+          </form>
+        ) : selectedForm === "short" ? (
+          <form>
+            <div className="truckSize">
+              <TruckSize />
+            </div>
+            <div className="movers">
+              <MoversQuantity />
+            </div>
+            <div className="hours">
+              <Hours />
+            </div>
+          </form>
+        ) : null}
       </form>
     </FormProvider>
   );
